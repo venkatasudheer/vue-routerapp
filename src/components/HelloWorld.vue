3c35b07fbb5883ea9c91befb2ab93412ed104615
venkatasudheer <venkatasudheermanda@gmail.com>
2019-08-27 21:55:30 +0530

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hello">
+       <p>hello this is Manda venkata sudheer kumar</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+}
+</script>
+
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    }
+  ]
+})
